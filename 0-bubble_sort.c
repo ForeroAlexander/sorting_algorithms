9ab6872ad27944109a689b8d6a3ea9ec19e45109
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-06-10 19:17:51 -0500

    [bubble_sort] Done!

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,4 +4,22 @@
  *@array: list of numbers
  *@size: lenght of the Array
  *Return: Sorted array*/
-void bubble_sort()
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, max_complex;
+	int tmp;
+	for (max_complex = size - 1; max_complex > 0 ; max_complex--)
+	{
+		for (i = 0; i < max_complex; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+			}
+		}
+
+	}
+}
