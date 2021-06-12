925654da316f6486d075ab7744c8a52ba8c3b8ae
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-06-12 12:19:55 -0500

    [insertion_sort] Done!

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ *insertion_sort_list - Sorting algoritm (insertion sort)
+ *@list: double pointer of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *previus, *current, *tmp, *bigger;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	bigger = previus = *list;
+	current = previus->next;
+	while (bigger)
+	{
+		while (current)
+		{
+			if (previus->n > current->n)
+			{
+				if (previus->prev == NULL)
+					*list = current;
+				if (previus->prev)
+					previus->prev->next = current;
+				current->prev = previus->prev;
+				previus->prev = current;
+				previus->next = current->next;
+				if (previus->next)
+					previus->next->prev = previus;
+				current->next = previus;
+
+				tmp = previus;
+				previus = current;
+				current = tmp;
+				print_list(*list);
+			}
+			previus = previus->next;
+			current = current->next;
+		}
+		bigger = bigger->next;
+		previus = *list;
+		current = previus->next;
+	}
+}
