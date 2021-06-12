0df65b857495fad67825f4322b795ad6bdfab7b5
ForeroAlexander <foreroalex26@gmail.com>
2021-06-12 12:54:10 -0500

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection sort - is a sorting algorithms, specifically an
+ * in-place comparison sort
+ *@array: array
+ *@size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, p_min;
+    int tmp;
+
+    for (i = 0; i < (size - 1); i++)
+    {
+        p_min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[p_min])
+                p_min = j;
+        }
+        if (p_min != i)
+        {
+            tmp = array[p_min];
+            array[p_min] = array[i];
+            array[i] = tmp;
+            print_array(array, size);
+        }
+    }
+}
