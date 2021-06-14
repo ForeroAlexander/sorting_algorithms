2730da11a6fa660239e6e10c83610fa13338543d
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-06-14 15:27:20 -0500

    [insertion] update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *previus, *current, *tmp, *bigger;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL)
 		return;
 	bigger = previus = *list;
 	current = previus->next;
