5a85f2b2296763b779a376623b9d31fe6fefb89e
ForeroAlexander <foreroalex26@gmail.com>
2021-06-14 11:21:48 -0500

    bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, max_complex;
 	int tmp;
 
+        if (array == NULL || size == 0 || size == 1)
+            return;
+
 	for (max_complex = size - 1; max_complex > 0 ; max_complex--)
 	{
 		for (i = 0; i < max_complex; i++)
