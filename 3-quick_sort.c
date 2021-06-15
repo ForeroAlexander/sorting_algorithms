02f653ed52741839abcb71b56f1d880d89203ea9
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-06-15 14:15:39 -0500

    [quick_sort] update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,48 +1,53 @@
 #include "sort.h"
 
 /**
- *quick_sort_recursive - Quick sort algoritm
- *@array: List of numbers
- *@start: dinamic variable
- *@end: dinamic variable
- *@size: size of the array
+ * quick_sort_recursive - Quick sort algoritm
+ *
+ * @array: list of numbers
+ * @start: where start
+ * @end: where end
+ * @size: size of the array
  */
 void quick_sort_recursive(int *array, int start, int end, size_t size)
 {
-	int tmp, pivote, start_aux, end_aux;
-
-	if (start >= end)
+	if (start >= end || size <= 1)
 		return;
 
-	pivote = array[(start + end) / 2];
-	start_aux = start;
-	end_aux = end;
+	int start_aux, j, pivote, tmp;
 
-	do {
-		while (array[start_aux] < pivote && start_aux < end)
-			start_aux++;
-		while (array[end_aux] > pivote && end_aux > start)
-			end_aux--;
-		if (start_aux <= end_aux)
+	pivote = end;
+	start_aux = start - 1;
+	j = start;
+	while (j <= end - 1)
+	{
+		if (array[j] < array[pivote])
 		{
-			tmp = array[start_aux];
-			array[start_aux] = array[end_aux];
-			array[end_aux] = tmp;
 			start_aux++;
-			end_aux++;
-			print_array(array, size);
+			tmp = array[start_aux];
+			array[start_aux] = array[j];
+			array[j] = tmp;
+			if (array[j] != array[start_aux])
+				print_array(array, size);
 		}
-	} while (start_aux <= end_aux);
-	if (start < end_aux)
-		quick_sort_recursive(array, start, end_aux, size);
-	if (end > start_aux)
-		quick_sort_recursive(array, start_aux, end, size);
+		j++;
+	}
+	if (array[start_aux + 1] != array[end])
+	{
+		tmp = array[start_aux + 1];
+		array[start_aux + 1] = array[end];
+		array[end] = tmp;
+		print_array(array, size);
+	}
+	pivote = start_aux + 1;
+	quick_sort_recursive(array, start, pivote - 1, size);
+	quick_sort_recursive(array, pivote + 1, end, size);
 }
 
 /**
- *quick_sort - Call the Quick sort algoritm
- *@array: List of numbers
- *@size: Size of the list of numbers
+ * quick_sort - Call the quick sort recursive
+ *
+ * @array: list of integers
+ * @size: size of the array
  */
 void quick_sort(int *array, size_t size)
 {
